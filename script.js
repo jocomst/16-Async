561df7f482b80e5569af018296f29fd336332530
John <johncomstock9716@gmail.com>
2022-04-04 22:53:32 -0400

    fixes

--- script.js
+++ script.js
@@ -3,6 +3,30 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderError = function (message) {
+  countriesContainer.insertAdjacentText('beforeend', message);
+};
+
+const renderCountry = function (data, className = '') {
+  const html = `<article class="country ${className}">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.official}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} million people</p>
+    // <p class="country__row"><span>🗣️</span>${
+      Object.values(data.languages)[0]
+    }</p>
+    // <p class="country__row"><span>💰</span>${
+      Object.values(data.currencies)[0].name
+    }</p>
+  </div>
+</article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+};
+
 ///////////////////////////////////////
 //https://restcountries.com/v2/
 // const getCountryData = function (country) {
@@ -35,7 +59,7 @@ const countriesContainer = document.querySelector('.countries');
 //     countriesContainer.style.opacity = 1;
 //   });
 // };
-
+/*
 const renderCountry = function (data, className = '') {
   const html = `<article class="country ${className}">
   <img class="country__img" src="${data.flags.png}" />
@@ -89,5 +113,102 @@ const getCountryAndNeighbor = function (country) {
     });
   });
 };
-
 getCountryAndNeighbor('mexico');
+
+*/
+
+//https://restcountries.com/v2/
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+
+const request = fetch(`https://restcountries.com/v3.1/name/usa`);
+
+//promise are a container for a future value
+// promises are time sensitive and can hold different states
+
+//pending, settled (fulfilled, rejected)
+// const getCountryData = function (count]ry) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+const getJSON = function (url, error = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(`${error} ${response.status}`);
+    }
+    return response.json();
+  });
+};
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(`Country not found ${response.status}`);
+//       }
+//       return response.json();
+//     })
+//     .then(data => {
+//       renderCountry(data[0]);
+//       // const neighbor = data[0].borders[0];
+//       const neighbor = 'sljfsdf';
+
+//       if (!neighbor) return;
+
+//       //country 2
+//       return fetch(`https://restcountries.com/v3.1/alpha/${neighbor}`);
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//       renderCountry(data[0], 'neighbour');
+//     })
+//     .catch(err => {
+//       console.error(`${err} ***`);
+//       renderError('Something went wrong ***');
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
+const getCountryData = function (country) {
+  getJSON(`https://restcountries.com/v3.1/name/${country}`, 'Country not found')
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      // const neighbor = 'sljfsdf';
+      if (!neighbour) throw new Error('No neighbor found!');
+
+      //country 2
+      return getJSON(
+        `https://restcountries.com/v3.1/alpha/${neighbour}`,
+        'Country not found'
+      );
+    })
+    .then(data => {
+      renderCountry(data[0], 'neighbour');
+    })
+    .catch(err => {
+      console.error(`${err.message} ***`);
+      renderError('Something went wrong ***');
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+btn.addEventListener('click', function () {
+  getCountryData('fiji');
+});
+
+// getCountryData('sdfgjdkf');
